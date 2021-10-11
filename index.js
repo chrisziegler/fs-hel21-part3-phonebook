9f537d9bf09bfb2dace6eecad25aa953919339b6
Chris Ziegler <chrisrziegler@gmail.com>
2021-10-11 01:00:03 -0700

    Update routes with error handling and full databse integration, add delete and put routes, write request test for REST client

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 // const path = require('path')
@@ -5,15 +6,15 @@ const app = express()
 // const fs = require('fs')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 
 morgan.token('info', req => JSON.stringify(req.info))
-
-// app.use(favicon(path.join(__dirname + '/public/images/favicon.ico')))
-app.use(express.json())
-app.use(express.static('build'))
 // const appLogStream = fs.createWriteStream(path.join(__dirname, 'app.log'))
+
+app.use(express.static('build'))
+app.use(express.json())
 app.use(
   morgan(
     ':method :url :status :res[content-length] :response-time ms :info',
@@ -21,96 +22,82 @@ app.use(
   ),
 )
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
-// Index Route
-// Needed by heroku until add express.static('build')
-// app.get('/', (req, res) => {
-//   res.send(
-//     `<div style='text-align: center; margin-top: 5rem; font-size: 1.5rem'>Go to <a href="/api/persons">/api/persons</a> for api</div>`,
-//   )
-// })
-
 // GET Info Route
-app.get('/info', (req, res) => {
+app.get('/info', async (req, res) => {
+  const entries = await Person.find({}).then(people => people.length)
+
   res.send(
-    `<div style="margin: 16px 0 0 8px">
-    <p>Phonebook has info for ${persons.length} people</p>
+    `<div style="margin: 16px 0 0 8px; font-size: 1.3rem">
+    <p>Phonebook has info for <span style="color: dodgerblue">${entries}</span> people</p>
     <p>${new Date()}</p>
     </div>`,
   )
 })
 
 // GET All Route
-app.get('/api/persons', (req, res) => {
-  res.status(200).json(persons)
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then(notes => {
+      res.json(notes)
+    })
+    .catch(error => next(error))
 })
 
 // SHOW Route
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.filter(p => p.id === id)
-
-  if (!person.length) {
-    return res.status(404).json({ error: 'person not found' })
-  }
-  res.status(200).json(person)
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  return Math.floor(Math.random() * 100000)
-}
-
-// CREATE Route
-app.post('/api/persons', (req, res) => {
-  const { body } = req
+// CREATE ROUTE
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
   req.info = body
+
   if (!body.name || !body.number) {
-    return res
-      .status(400)
-      .json({ error: 'request must include name and number' })
-  } else if (persons.some(p => p.name === body.name)) {
-    return res.status(400).json({ error: 'name must be unique' })
+    return res.status(400).json({ error: 'name or number missing' })
   }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
   const person = {
-    id: generateId(),
     name: body.name,
     number: body.number,
   }
-  persons = persons.concat(person)
-  res.json(person)
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
-// DESTROY Route
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const exists = persons.some(p => p.id === id)
-  if (!exists) {
-    return res.status(404).json({ error: 'person not found' })
-  }
-  persons = persons.filter(p => p.id !== id)
-  res.sendStatus(204)
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => res.status(204).end())
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -118,6 +105,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const [, , password, name, number] = process.argv
+
+const url = `mongodb+srv://cz-fs-helsinki:${password}@cluster0.otgwe.mongodb.net/phone-app-2021?retryWrites=true&w=majority`
+mongoose.connect(url)
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>',
+  )
+  console.log(
+    'Arguemnts to add entries to phonebook: <password> <name> <number>',
+  )
+  process.exit(1)
+}
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+    process.exit(0)
+  })
+} else {
+  const person = new Person({
+    name,
+    number,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
